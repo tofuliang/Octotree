1bc8d69c7de9847c20e0f66d750e57c001505728
tofuliang <tofuliang@gmail.com>
2021-12-09 14:49:45 +0800

    test auto upgrade

--- deal.sh
+++ deal.sh
@@ -1,15 +1,18 @@
-#!/bin/zsh
+#!/bin/bash
 
 dir=$1
 cd $dir
 rm  -fr _metadata manifest.fingerprint ../src
-sed -i 's/function\s\([a-z]\{1,\}([a-z]\{1,\})\){return [a-z]\{1,\}===[a-z]\{1,\}}/function \1{return !0}/g' content.js
+sed -i.bak 's/function\s\([a-z]\{1,\}([a-z]\{1,\})\){return [a-z]\{1,\}===[a-z]\{1,\}}/function \1{return !0}/g' content.js
+patchResult=$(diff content.js.bak content.js |grep -c -e "^<")
+rm content.js.bak
 sed -i '/differential_fingerprint/d' manifest.json
 sed -i '/key/d' manifest.json
 sed -i 's/"version":\s\{1,\}"\(.*\)"/"version": "99.\1"/' manifest.json
 sed -i 's/"update_url":\s\{1,\}"\(.*\)"/"update_url": "https:\/\/raw.githubusercontent.com\/tofuliang\/octotree\/master\/update.xml"/' manifest.json
 cd ..
 
-ver=$dir[1,-3]
+ver=$2
 sed -i "s/\"\sversion=\"[0-9\.]*\"/\" version=\"99.${ver}\"/" update.xml
 mv $dir src
+echo $patchResult
